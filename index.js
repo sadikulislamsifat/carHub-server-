233d970c5f2b027fe38ac82b903494a2c0168504
Sadikul Islam <sh.sifatallrafi.08@gmail.com>
2022-11-30 21:56:34 +0600

    paymet data load from stirke

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const stripe = require('stripe')(process.env.SECRET_KEY);
 const { MongoClient, ServerApiVersion , ObjectId} = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
@@ -35,6 +36,8 @@ async function run(){
         const allCarsData = client.db('carsHub').collection('carsCollection');
         const reviewCollection = client.db('carsHub').collection('reviews');
         const usersCollection = client.db('carsHub').collection('users');
+        const bookingCollection = client.db('carsHub').collection('bookings');
+        const paymentsCollection = client.db('carsHub').collection('payments');
 
         app.get('/reviews', async(req, res) => {
             let query = {};
@@ -48,6 +51,60 @@ async function run(){
             res.send(reviews);
         });
 
+        app.get('/bookingnow/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const paymentOrder = await bookingCollection.findOne(query);
+            res.send(paymentOrder)
+
+        })
+
+        // // payment option
+		// app.post("/create-payment-intent", async (req, res) => {
+		// 	const order = req.body;
+		// 	// console.log(order);
+		// 	const price = order.price;
+		// 	const amount = price * 100;
+		// 	// console.log(price);
+		// 	const paymentIntent = await stripe.paymentIntents.create({
+		// 		currency: "usd",
+		// 		amount: amount,
+		// 		payment_method_types: ["card"],
+		// 	});
+		// 	res.send({
+		// 		clientSecret: paymentIntent.client_secret,
+		// 	});
+		// });
+
+
+		// app.post("/payments", async (req, res) => {
+		// 	const payment = req.body;
+		// 	const result = await paymentsCollection.insertOne(payment);
+		// 	const id = payment.order;
+		// 	const filter = { _id: ObjectId(id) };
+		// 	const updatedDoc = {
+		// 		$set: {
+		// 			paid: true,
+		// 			transactionId: payment.transactionId,
+		// 		},
+		// 	};
+		// 	const updatedResult = await ordersCollections.updateOne(
+		// 		filter,
+		// 		updatedDoc
+		// 	);
+		// 	res.send(result);
+		// });
+
+
+
+		// payment option
+
+        app.post('/bookings', async(req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
+
         app.get('/allcars', async(req, res) => {
             const query = {};
             const allCars = await allCarsData.find(query).toArray();
@@ -59,6 +116,13 @@ async function run(){
             const result = await allCarsData.deleteOne(query);
             res.send(result)
         });
+
+        app.get('/bookings/:email', async(req, res) => {
+            const email = req.params.email;
+            const query = {email: email};
+            const myBooking = await bookingCollection.find(query).toArray();
+            res.send(myBooking)
+        })
         
         app.get('/cars/:id', (req, res) => {
             const id = req.params.id;
