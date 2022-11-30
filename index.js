dce77d4b8f4d99998e288a7680a99b0e8f27e808
Sadikul Islam <sh.sifatallrafi.08@gmail.com>
2022-11-30 14:26:06 +0600

    all user data load from mongodb

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { MongoClient, ServerApiVersion , ObjectId} = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -12,7 +13,8 @@ app.use(express.json());
 
 const blogItems = require('./blog.json');
 const brands = require('./categorys.json');
-const carsData = require('./cars.json')
+const carsData = require('./cars.json');
+const { query } = require('express');
 
 
 
@@ -45,6 +47,18 @@ async function run(){
             const reviews = await cursor.toArray();
             res.send(reviews);
         });
+
+        app.get('/allcars', async(req, res) => {
+            const query = {};
+            const allCars = await allCarsData.find(query).toArray();
+            res.send(allCars)
+        });
+        app.delete('/allcars/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await allCarsData.deleteOne(query);
+            res.send(result)
+        });
         
         app.get('/cars/:id', (req, res) => {
             const id = req.params.id;
@@ -71,11 +85,57 @@ async function run(){
             res.send(result)
         });
 
+        app.get('/jwt', async(req, res) => {
+            const email = req.query.email;
+            const query ={email: email}
+            const user = await usersCollection.findOne(query);
+            // if(user){
+            //     const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'});
+            //     return res.send({accesToken: token})
+            // }
+            const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'});
+            //     return res.send({accesToken: token})
+            // res.status(403).send({accessToken : ''})
+            res.send(token)
+           
+        });
+        app.get('/allSellers', async(req, res) => {
+            // const seller = req.params.seller;
+            const query = {seller: true};
+            const allSeller = await usersCollection.find(query).toArray();
+            res.send(allSeller)
+        })
+        app.get('/allBuyers', async(req, res) => {
+            // const seller = req.params.seller;
+            const query = {seller: false};
+            const allBuyers = await usersCollection.find(query).toArray();
+            res.send(allBuyers)
+        })
+
+        app.get('/users', async(req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users)
+        })
+
         app.post('/users', async(req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
             res.send(result)
 
+        });
+
+        app.put('/users/admin/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id) }
+            const options = {upsert: true};
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result)
         })
 
     }
